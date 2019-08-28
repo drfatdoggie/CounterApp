23dc24a6149076ef698e0b553a67deb047a73dcd
Megan Keesee <mrkeesee1s@gmail.com>
2019-08-28 12:12:44 -0700

    First commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import CounterApp from './src/CounterApp';
+
+
+//redux start
+
+const initialState = {
+  counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type)
+  {
+    case 'INCREASE_COUNTER':
+      return {counter: state.counter + 1}
+    case 'DECREASE_COUNTER':
+      return {counter: state.counter - 1}
+  }
+  return state;
+}
+
+const store = createStore(reducer);
+
+//redux end
+
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <CounterApp />
+      </Provider>
+    );
+  } 
+}
+
+export default App;
+
